456506fdacd1fd0adc9af90a67dbb7fc1c368222
Danny Felipe <dannyfmello@gmail.com>
2016-05-01 14:16:38 -0300

    LERIP: método para listar todos os IPs disponíveis (virtual, físico)

--- JShare141167/src/br/dagostini/exemplos/LerIp.java
+++ JShare141167/src/br/dagostini/exemplos/LerIp.java
@@ -1,7 +1,11 @@
 package br.dagostini.exemplos;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class LerIp {
 
@@ -16,6 +20,37 @@ public class LerIp {
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// método para listar todos os IP
+	public List<String> ListIp() {
+		List<String> listaIP = new ArrayList<>();
+		
+		try {
+			Enumeration<NetworkInterface> net = NetworkInterface.getNetworkInterfaces();
+			while (net.hasMoreElements()) {
+				NetworkInterface networkInterface = (NetworkInterface) net.nextElement();
+				
+				if (networkInterface.isUp()) {
+					Enumeration<InetAddress> netAddress = networkInterface.getInetAddresses();
+					
+					while (netAddress.hasMoreElements()) {
+						InetAddress inetAddress = (InetAddress) netAddress.nextElement();
+						
+						String ip = inetAddress.getHostAddress();
+						
+						if (ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+							listaIP.add(ip.trim());
+						}
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaIP;
+		
 	}
 
 	public static void main(String[] args) {
