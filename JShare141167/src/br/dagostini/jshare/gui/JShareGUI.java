4389c98b0d827209a5e46f04a846b953b651ae98
Danny Felipe <dannyfmello@gmail.com>
2016-05-02 21:00:05 -0300

    JSHAREGUI: ajuste na interface da tela.

--- JShare141167/src/br/dagostini/jshare/gui/JShareGUI.java
+++ JShare141167/src/br/dagostini/jshare/gui/JShareGUI.java
@@ -108,11 +108,10 @@ public class JShareGUI extends JFrame implements IServer {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWidths = new int[] { 308, 0, 0, 0 };
 		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_contentPane.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-				0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.columnWeights = new double[] { 1.0,
+				0.0, 1.0, Double.MIN_VALUE };
 		gbl_contentPane.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 				0.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
@@ -120,7 +119,6 @@ public class JShareGUI extends JFrame implements IServer {
 		JLabel lblUsurio = new JLabel("USUÁRIO:");
 		GridBagConstraints gbc_lblUsurio = new GridBagConstraints();
 		gbc_lblUsurio.anchor = GridBagConstraints.WEST;
-		gbc_lblUsurio.gridwidth = 13;
 		gbc_lblUsurio.insets = new Insets(0, 0, 5, 5);
 		gbc_lblUsurio.gridx = 0;
 		gbc_lblUsurio.gridy = 0;
@@ -129,32 +127,30 @@ public class JShareGUI extends JFrame implements IServer {
 		JLabel lblServidor = new JLabel("SERVIDOR:");
 		GridBagConstraints gbc_lblServidor = new GridBagConstraints();
 		gbc_lblServidor.anchor = GridBagConstraints.WEST;
-		gbc_lblServidor.gridwidth = 13;
-		gbc_lblServidor.insets = new Insets(0, 0, 5, 5);
-		gbc_lblServidor.gridx = 14;
+		gbc_lblServidor.insets = new Insets(0, 0, 5, 0);
+		gbc_lblServidor.gridx = 2;
 		gbc_lblServidor.gridy = 0;
 		contentPane.add(lblServidor, gbc_lblServidor);
 
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.gridheight = 12;
-		gbc_panel.gridwidth = 13;
 		gbc_panel.insets = new Insets(0, 0, 5, 5);
 		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 1;
 		contentPane.add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWidths = new int[] { 0, 0, 0, 0, 0, 0 };
 		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+		gbl_panel.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0, 0.0,
 				Double.MIN_VALUE };
 		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
 
 		JLabel lblNome = new JLabel("Nome:");
 		GridBagConstraints gbc_lblNome = new GridBagConstraints();
-		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.anchor = GridBagConstraints.WEST;
 		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNome.gridx = 0;
 		gbc_lblNome.gridy = 0;
@@ -162,7 +158,7 @@ public class JShareGUI extends JFrame implements IServer {
 
 		txtF_nome = new JTextField();
 		GridBagConstraints gbc_txtF_nome = new GridBagConstraints();
-		gbc_txtF_nome.gridwidth = 11;
+		gbc_txtF_nome.gridwidth = 3;
 		gbc_txtF_nome.insets = new Insets(0, 0, 5, 5);
 		gbc_txtF_nome.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtF_nome.gridx = 1;
@@ -172,7 +168,7 @@ public class JShareGUI extends JFrame implements IServer {
 
 		JLabel lblIpServer = new JLabel("IP Server:");
 		GridBagConstraints gbc_lblIpServer = new GridBagConstraints();
-		gbc_lblIpServer.anchor = GridBagConstraints.EAST;
+		gbc_lblIpServer.anchor = GridBagConstraints.WEST;
 		gbc_lblIpServer.insets = new Insets(0, 0, 5, 5);
 		gbc_lblIpServer.gridx = 0;
 		gbc_lblIpServer.gridy = 1;
@@ -180,7 +176,6 @@ public class JShareGUI extends JFrame implements IServer {
 
 		txtF_ipserver = new JTextField();
 		GridBagConstraints gbc_txtF_ipserver = new GridBagConstraints();
-		gbc_txtF_ipserver.gridwidth = 6;
 		gbc_txtF_ipserver.insets = new Insets(0, 0, 5, 5);
 		gbc_txtF_ipserver.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtF_ipserver.gridx = 1;
@@ -192,16 +187,15 @@ public class JShareGUI extends JFrame implements IServer {
 		GridBagConstraints gbc_lblPorta = new GridBagConstraints();
 		gbc_lblPorta.anchor = GridBagConstraints.EAST;
 		gbc_lblPorta.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPorta.gridx = 7;
+		gbc_lblPorta.gridx = 2;
 		gbc_lblPorta.gridy = 1;
 		panel.add(lblPorta, gbc_lblPorta);
 
 		txtF_Uporta = new JTextField();
 		GridBagConstraints gbc_txtF_Uporta = new GridBagConstraints();
-		gbc_txtF_Uporta.gridwidth = 4;
 		gbc_txtF_Uporta.insets = new Insets(0, 0, 5, 5);
 		gbc_txtF_Uporta.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtF_Uporta.gridx = 8;
+		gbc_txtF_Uporta.gridx = 3;
 		gbc_txtF_Uporta.gridy = 1;
 		panel.add(txtF_Uporta, gbc_txtF_Uporta);
 		txtF_Uporta.setColumns(10);
@@ -215,7 +209,7 @@ public class JShareGUI extends JFrame implements IServer {
 		});
 		GridBagConstraints gbc_btn_conectar = new GridBagConstraints();
 		gbc_btn_conectar.insets = new Insets(0, 0, 5, 0);
-		gbc_btn_conectar.gridx = 12;
+		gbc_btn_conectar.gridx = 4;
 		gbc_btn_conectar.gridy = 1;
 		panel.add(btn_conectar, gbc_btn_conectar);
 
@@ -228,7 +222,7 @@ public class JShareGUI extends JFrame implements IServer {
 
 		JLabel lblArquivo = new JLabel("Arquivo:");
 		GridBagConstraints gbc_lblArquivo = new GridBagConstraints();
-		gbc_lblArquivo.anchor = GridBagConstraints.EAST;
+		gbc_lblArquivo.anchor = GridBagConstraints.WEST;
 		gbc_lblArquivo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblArquivo.gridx = 0;
 		gbc_lblArquivo.gridy = 3;
@@ -236,7 +230,7 @@ public class JShareGUI extends JFrame implements IServer {
 
 		txtF_arquivo = new JTextField();
 		GridBagConstraints gbc_txtF_arquivo = new GridBagConstraints();
-		gbc_txtF_arquivo.gridwidth = 11;
+		gbc_txtF_arquivo.gridwidth = 3;
 		gbc_txtF_arquivo.insets = new Insets(0, 0, 5, 5);
 		gbc_txtF_arquivo.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtF_arquivo.gridx = 1;
@@ -253,13 +247,13 @@ public class JShareGUI extends JFrame implements IServer {
 		});
 		GridBagConstraints gbc_btn_Pesquisar = new GridBagConstraints();
 		gbc_btn_Pesquisar.insets = new Insets(0, 0, 5, 0);
-		gbc_btn_Pesquisar.gridx = 12;
+		gbc_btn_Pesquisar.gridx = 4;
 		gbc_btn_Pesquisar.gridy = 3;
 		panel.add(btn_Pesquisar, gbc_btn_Pesquisar);
 
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.gridwidth = 13;
+		gbc_scrollPane.gridwidth = 5;
 		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
@@ -276,14 +270,14 @@ public class JShareGUI extends JFrame implements IServer {
 		});
 		GridBagConstraints gbc_btn_Download = new GridBagConstraints();
 		gbc_btn_Download.insets = new Insets(0, 0, 5, 0);
-		gbc_btn_Download.gridx = 12;
+		gbc_btn_Download.gridx = 4;
 		gbc_btn_Download.gridy = 5;
 		panel.add(btn_Download, gbc_btn_Download);
 
 		JScrollPane scrollPane_1 = new JScrollPane();
 		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
 		gbc_scrollPane_1.gridheight = 3;
-		gbc_scrollPane_1.gridwidth = 13;
+		gbc_scrollPane_1.gridwidth = 5;
 		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane_1.gridx = 0;
 		gbc_scrollPane_1.gridy = 6;
@@ -295,16 +289,15 @@ public class JShareGUI extends JFrame implements IServer {
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.gridheight = 12;
-		gbc_panel_1.gridwidth = 13;
 		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 14;
+		gbc_panel_1.gridx = 2;
 		gbc_panel_1.gridy = 1;
 		contentPane.add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel_1.columnWidths = new int[] { 0, 0, 0, 0, 0 };
 		gbl_panel_1.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panel_1.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_1.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		gbl_panel_1.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 				Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
@@ -319,7 +312,6 @@ public class JShareGUI extends JFrame implements IServer {
 
 		cBx_endip = new JComboBox();
 		GridBagConstraints gbc_cBx_endip = new GridBagConstraints();
-		gbc_cBx_endip.gridwidth = 3;
 		gbc_cBx_endip.insets = new Insets(0, 0, 5, 5);
 		gbc_cBx_endip.fill = GridBagConstraints.HORIZONTAL;
 		gbc_cBx_endip.gridx = 1;
@@ -330,7 +322,7 @@ public class JShareGUI extends JFrame implements IServer {
 		GridBagConstraints gbc_lblPorta_1 = new GridBagConstraints();
 		gbc_lblPorta_1.anchor = GridBagConstraints.EAST;
 		gbc_lblPorta_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPorta_1.gridx = 4;
+		gbc_lblPorta_1.gridx = 2;
 		gbc_lblPorta_1.gridy = 0;
 		panel_1.add(lblPorta_1, gbc_lblPorta_1);
 
@@ -338,7 +330,7 @@ public class JShareGUI extends JFrame implements IServer {
 		GridBagConstraints gbc_txtF_Sporta = new GridBagConstraints();
 		gbc_txtF_Sporta.insets = new Insets(0, 0, 5, 0);
 		gbc_txtF_Sporta.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtF_Sporta.gridx = 5;
+		gbc_txtF_Sporta.gridx = 3;
 		gbc_txtF_Sporta.gridy = 0;
 		panel_1.add(txtF_Sporta, gbc_txtF_Sporta);
 		txtF_Sporta.setColumns(10);
@@ -346,7 +338,7 @@ public class JShareGUI extends JFrame implements IServer {
 		JScrollPane scrollPane_2 = new JScrollPane();
 		GridBagConstraints gbc_scrollPane_2 = new GridBagConstraints();
 		gbc_scrollPane_2.gridheight = 10;
-		gbc_scrollPane_2.gridwidth = 6;
+		gbc_scrollPane_2.gridwidth = 4;
 		gbc_scrollPane_2.insets = new Insets(0, 0, 5, 0);
 		gbc_scrollPane_2.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane_2.gridx = 0;
@@ -356,19 +348,6 @@ public class JShareGUI extends JFrame implements IServer {
 		txtA_Status = new JTextArea();
 		scrollPane_2.setViewportView(txtA_Status);
 
-		btn_IniciarServico = new JButton("Iniciar serviço");
-		btn_IniciarServico.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// invoca o método para que o serviço do servidor seja iniciado.
-				startService();
-			}
-		});
-		GridBagConstraints gbc_btn_IniciarServico = new GridBagConstraints();
-		gbc_btn_IniciarServico.insets = new Insets(0, 0, 0, 5);
-		gbc_btn_IniciarServico.gridx = 4;
-		gbc_btn_IniciarServico.gridy = 11;
-		panel_1.add(btn_IniciarServico, gbc_btn_IniciarServico);
-
 		btn_PararServico = new JButton("Parar serviço");
 		btn_PararServico.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -377,8 +356,22 @@ public class JShareGUI extends JFrame implements IServer {
 				stopService();
 			}
 		});
+		
+				btn_IniciarServico = new JButton("Iniciar serviço");
+				btn_IniciarServico.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// invoca o método para que o serviço do servidor seja iniciado.
+						startService();
+					}
+				});
+				GridBagConstraints gbc_btn_IniciarServico = new GridBagConstraints();
+				gbc_btn_IniciarServico.anchor = GridBagConstraints.EAST;
+				gbc_btn_IniciarServico.insets = new Insets(0, 0, 0, 5);
+				gbc_btn_IniciarServico.gridx = 1;
+				gbc_btn_IniciarServico.gridy = 11;
+				panel_1.add(btn_IniciarServico, gbc_btn_IniciarServico);
 		GridBagConstraints gbc_btn_PararServico = new GridBagConstraints();
-		gbc_btn_PararServico.gridx = 5;
+		gbc_btn_PararServico.gridx = 3;
 		gbc_btn_PararServico.gridy = 11;
 		panel_1.add(btn_PararServico, gbc_btn_PararServico);
 		
